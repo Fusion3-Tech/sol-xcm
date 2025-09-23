417757f7ad44a93b07efdbe9b4fa6bdf1d43eae7
Szegoo <sakacszergej@gmail.com>
2025-09-23 18:11:54 +0200

    ScaleCodec tests

---
+++ src/contracts/README.md
@@ -0,0 +1,7 @@
+### Running Tests
+
+To run all the tests in the project, execute the following command:
+
+```shell
+npx hardhat test
+```
---
+++ src/contracts/src/ScaleCodecHarness.sol
@@ -0,0 +1,70 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./ScaleCodec.sol";
+
+contract ScaleCodecHarness {
+    function u8(uint8 v) external pure returns (bytes memory) {
+        return ScaleCodec.u8(v);
+    }
+
+    function u16LE(uint16 v) external pure returns (bytes memory) {
+        return ScaleCodec.u16LE(v);
+    }
+
+    function u32LE(uint32 v) external pure returns (bytes memory) {
+        return ScaleCodec.u32LE(v);
+    }
+
+    function u64LE(uint64 v) internal pure returns (bytes memory) {
+        return ScaleCodec.u64LE(v);
+    }
+
+    function u128LE(uint128 v) internal pure returns (bytes memory) {
+        return ScaleCodec.u128LE(v);
+    }
+
+    // --- Compact-uint per Substrate SCALE spec (covers up to uint256) ---
+
+    function compactUint(uint256 x) internal pure returns (bytes memory) {
+        return ScaleCodec.compactUint(x);
+    }
+
+    function compactU128(uint128 x) internal pure returns (bytes memory) {
+        return ScaleCodec.compactU128(x);
+    }
+
+    function compactU32(uint32 x) internal pure returns (bytes memory) {
+        return ScaleCodec.compactU32(x);
+    }
+
+    // --- Vec<u8> / Bytes ---
+    function vecU8(bytes memory data) internal pure returns (bytes memory) {
+        return ScaleCodec.vecU8(data);
+    }
+
+    // --- bool ---
+    function boolean(bool v) internal pure returns (bytes memory) {
+        return ScaleCodec.boolean(v);
+    }
+
+    // --- Option<T> (some = 0x01 + T, none = 0x00) ---
+    function optionNone() internal pure returns (bytes memory) {
+        return ScaleCodec.optionNone();
+    }
+
+    function optionSome(bytes memory encodedT) internal pure returns (bytes memory) {
+        return ScaleCodec.optionSome(encodedT);
+    }
+
+    // --- MultiAddress::Id(AccountId32) ---
+    // Variant index 0x00 followed by 32B AccountId.
+    function multiAddressId32(bytes32 accountId32) internal pure returns (bytes memory) {
+        return ScaleCodec.multiAddressId32(accountId32);
+    }
+
+    // --- Call index (2 bytes: pallet, call) ---
+    function callIndex(uint8 palletIndex, uint8 callIndex_) internal pure returns (bytes memory) {
+        return ScaleCodec.callIndex(palletIndex, callIndex_);
+    }
+}
