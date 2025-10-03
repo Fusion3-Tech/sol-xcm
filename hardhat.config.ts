47a6aa9057ad219d2799683cd008ac16b18d2ee5
Igor D. <igordobricc@gmail.com>
2025-10-03 12:11:20 +0200

    Re-integrate Hardhat, update configs and dependencies
    Moved Hardhat configuration and related dependencies for Solidity contract testing and development to root folder. Updated TypeScript and package settings, improved call encoder code generation, and removed redundant contract and config files from src/contracts.

---
+++ hardhat.config.ts
@@ -0,0 +1,20 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import hardhatToolboxViem from "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  plugins: [hardhatToolboxViem],
+  paths: { 
+    sources: "src/contracts/src/",
+    tests: "src/contracts/test/",
+    artifacts: "src/contracts/artifacts/",
+    cache: "src/contracts/cache/"
+  },
+  test: {
+    solidity: {
+      timeout: 60000
+    }
+  },
+};
+
+export default config;
