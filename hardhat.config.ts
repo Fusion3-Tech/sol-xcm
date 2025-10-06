a97feb364e7a0e8d582a15ed90f099cf35224b94
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2025-10-06 21:31:46 +0200

    Test encoding for Balances (#13)
    * Test encoding for Balances
    
    * change naming
    
    * fix
    
    * Integration Tests
    
    Test coverage for integration
    
    * CLeanup of tests
    
    Removed redundant tests and added a few more edge cases
    
    * Re-integrate Hardhat, update configs and dependencies
    
    Moved Hardhat configuration and related dependencies for Solidity contract testing and development to root folder. Updated TypeScript and package settings, improved call encoder code generation, and removed redundant contract and config files from src/contracts.
    
    * Update src/callEncoder.ts
    
    Co-authored-by: Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
    
    * remove unnecessary code
    
    ---------
    
    Co-authored-by: Igor D. <igordobricc@gmail.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,12 @@ import hardhatToolboxViem from "@nomicfoundation/hardhat-toolbox-viem";
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   plugins: [hardhatToolboxViem],
-  paths: { sources: "src/" }
+  paths: { 
+    sources: "src/contracts/src/",
+    tests: "src/contracts/test/",
+    artifacts: "src/contracts/artifacts/",
+    cache: "src/contracts/cache/"
+  },
 };
 
 export default config;
